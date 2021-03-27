76243ac5d21578efb434b8c9febeb0aa4ba0dd68
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-03-28 02:00:20 +0530

    Create Arcesium_interview_Ransom_note.cpp

---
+++ Arcesium_interview_Ransom_note.cpp
@@ -0,0 +1,123 @@
+/*  Harold is a kidnapper who wrote a ransom note, but now he is worried it will be traced back to him through his handwriting. He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use only whole words available in the magazine. He cannot use substrings or concatenation to create the words he needs.
+
+Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+
+Example
+ magazine= "attack at dawn"  note= "Attack at dawn"
+
+The magazine has all the right words, but there is a case mismatch. The answer is No  */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the checkMagazine function below.
+void checkMagazine(vector<string> magazine, vector<string> note) {
+        unordered_map<string,int>st;
+         unordered_map<string,int>umap;
+        int cnt=0;
+        int flag=0;
+       for(int i=0;i<magazine.size();i++)
+       {
+             st[magazine[i]]++;
+       }
+       for(int i=0;i<note.size();i++)
+       {
+          umap[note[i]]++;
+       }
+       for(int i=0;i<note.size();i++)
+       {
+           if(st.find(note[i])!=st.end())
+           {
+               if(st[note[i]]<umap[note[i]])
+                 {  flag++;
+                     break;
+                 }
+           }
+           else {
+               flag++;
+               break;
+           }
+       }
+       if(flag)
+       cout<<"No"<<"\n";
+       else 
+       cout<<"Yes"<<"\n";
+      
+        
+       
+
+}
+
+int main()
+{
+    string mn_temp;
+    getline(cin, mn_temp);
+
+    vector<string> mn = split_string(mn_temp);
+
+    int m = stoi(mn[0]);
+
+    int n = stoi(mn[1]);
+
+    string magazine_temp_temp;
+    getline(cin, magazine_temp_temp);
+
+    vector<string> magazine_temp = split_string(magazine_temp_temp);
+
+    vector<string> magazine(m);
+
+    for (int i = 0; i < m; i++) {
+        string magazine_item = magazine_temp[i];
+
+        magazine[i] = magazine_item;
+    }
+
+    string note_temp_temp;
+    getline(cin, note_temp_temp);
+
+    vector<string> note_temp = split_string(note_temp_temp);
+
+    vector<string> note(n);
+
+    for (int i = 0; i < n; i++) {
+        string note_item = note_temp[i];
+
+        note[i] = note_item;
+    }
+
+    checkMagazine(magazine, note);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}

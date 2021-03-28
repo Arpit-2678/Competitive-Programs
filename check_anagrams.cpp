99ce1c5ac63e06dace7bacb31d472f5930054d2b
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-03-29 02:13:03 +0530

    Update check_anagrams.cpp

--- check_anagrams.cpp
+++ check_anagrams.cpp
@@ -3,18 +3,21 @@
 //  This is only the function.. Full question you can see from the link
 
 
-bool isAnagram(string a, string b){
+  bool isAnagram(string a, string b){
         
         int av=0,bv=0;
+        long long pav=0,pbv=0;
         for(int i=0;i<a.length();i++)
         {
             av+=(int)(a.at(i));
+            pav*=(int)(a.at(i));
         }
          for(int i=0;i<b.length();i++)
         {
             bv+=(int)(b.at(i));
+            pbv*=(int)(b.at(i));
         }
-        if(av==bv)
+        if(av==bv and pav==pbv)
         return true;
         else
         return false;

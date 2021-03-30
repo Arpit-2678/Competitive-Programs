e2c1e632c69c7f60b7a3580c0aea3b37efbf020d
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-03-30 19:19:25 +0530

    Update largest_subarray_with_0_sum.cpp

--- largest_subarray_with_0_sum.cpp
+++ largest_subarray_with_0_sum.cpp
@@ -8,37 +8,34 @@ int main()
   for(int i=0;i<n;i++)
   {
      cin>>arr[i];
-      cout<<arr[i]<<" ";
+     
     }
-    cout<<"\n"<<arr[0]<<" ";
+  
     for(int i=1;i<n;i++)
     {
       arr[i]=arr[i]+arr[i-1];
-      cout<<arr[i]<<" ";
+     
     }
-    cout<<"\n";
+    
   unordered_map<int,int>umap;
-  int max_len=-1;
+  int max_len=0;
   for(int i=0;i<n;i++)
   {
-    if(umap.count(arr[i]))
-    {
-      if(arr[i]==0)
+     if(arr[i]==0)
       {
         max_len=max(max_len,i+1);
       }
-      else
-      {
+    if(umap.count(arr[i]))
+    {
+     
+      
         max_len=max(max_len,i-umap[arr[i]]);
-      }
+      
     }
     else
       umap[arr[i]]=i;
   }
-  for(auto it:umap)
-  {
-    cout<<it.first<<" "<<it.second<<"\n";
-  }
+ 
   cout<<max_len<<"\n";
 
 }

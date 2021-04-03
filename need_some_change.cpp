2bb2549c0d4dd25971e09514f4f8960f90379663
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-04-03 22:58:00 +0530

    Create need_some_change.cpp

---
+++ need_some_change.cpp
@@ -0,0 +1,56 @@
+// LINK=https://practice.geeksforgeeks.org/problems/need-some-change/1/?category[]=Mathematical&category[]=Arrays&category[]=Strings&category[]=Mathematical&category[]=Arrays&category[]=Strings&problemStatus=unsolved&problemType=functional&page=1&sortBy=submissions&query=category[]Mathematicalcategory[]Arrayscategory[]StringsproblemStatusunsolvedproblemTypefunctionalpage1sortBysubmissionscategory[]Mathematicalcategory[]Arrayscategory[]Strings#
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*Function to swap array elements
+* arr : array input
+* sizeof_array : number of elements in array
+*/
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        
+        for(int i=0;i<n-2;i++)
+        {
+            swap(arr[i],arr[i+2]);
+        }
+        
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code to test swapElements
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int sizeof_array;
+	    cin >> sizeof_array;
+	    
+	    int arr[sizeof_array];
+	    
+	    for(int i = 0;i<sizeof_array;i++){
+	        cin >> arr[i];
+	    }
+        Solution obj;
+        // calling function to swap the array swap elements
+	    obj.swapElements(arr, sizeof_array);
+	    
+	    // Printing the modified array
+	    for(int i = 0;i<sizeof_array;i++){
+            cout << arr[i] << " ";
+        }
+	    
+	    cout << endl;
+	}
+	
+	return 0;
+}  // } Driver Code Ends

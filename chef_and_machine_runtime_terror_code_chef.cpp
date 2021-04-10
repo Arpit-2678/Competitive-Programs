6edcda74b8ce7841c20f8fbb258e2bd1c2ce47f2
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-04-10 18:54:54 +0530

    Create chef_and_machine_runtime_terror_code_chef.cpp

---
+++ chef_and_machine_runtime_terror_code_chef.cpp
@@ -0,0 +1,77 @@
+// LINK=https://www.codechef.com/RTR2021/problems/CMACHINE
+
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define test while(t--)
+#define pb push_back
+#define ub upper_bound
+#define lb lower_bound
+#define mp make_pair
+#define xxx exit(0)
+#define ain(n,arr) for(int i=0;i<n;i++){ cin>>arr[i];}     
+#define fs first
+#define sec second
+#define op(container) for(auto it: container) 
+using namespace std;
+const int inf = 1e9+7;
+
+//   for calculating a to power b...
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+//  for calculating gcd.. 
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+//  for converting string to uppercase....
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+//  for converting string to lowercase.... 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+ll fact(int n)
+{
+    if(n==0 or n==1)
+    return 1;
+    else
+    return n*fact(n-1);
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    test{
+        ll n;
+        cin>>n;
+        cout<<fact(n)<<"\n";
+    }
+    
+    
+    
+
+   
+
+    
+
+	return 0;
+}

d401353d39bd6e8b0da78e4ff0b21a9320743ca7
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-04-14 23:27:45 +0530

    Add files via upload

---
+++ balanced_parenthesis.cpp
@@ -0,0 +1,54 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool balancedParenthesis(string str)
+{
+    stack<char> s;
+    for (int i = 0; i < str.size(); i++)
+    {
+        char c = str[i];
+        if (c == '(' || c == '{' || c == '[')
+        {
+            s.push(c);
+        }
+        else if (c == ')')
+        {
+            if (s.empty() || s.top() != '(')
+            {
+                return false;
+            }
+            s.pop();
+        }
+        else if (c == '}')
+        {
+            if (s.empty() || s.top() != '{')
+            {
+                return false;
+            }
+            s.pop();
+        }
+        else if (c == ']')
+        {
+            if (s.empty() || s.top() != '[')
+            {
+                return false;
+            }
+            s.pop();
+        }
+    }
+    return s.empty();
+}
+int main()
+{
+    string str;
+    cin >> str;
+    bool ans = balancedParenthesis(str);
+    if (ans)
+        cout << "Balanced";
+    else
+        cout << "Not Balanced";
+
+    return 0;
+}

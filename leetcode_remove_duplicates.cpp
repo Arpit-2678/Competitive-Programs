354238c0192d8ea3aaa71214b4b29de7357e3269
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-04-18 13:56:24 +0530

    Create leetcode_remove_duplicates.cpp

---
+++ leetcode_remove_duplicates.cpp
@@ -0,0 +1,12 @@
+// LINK=https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        auto it=unique(nums.begin(),nums.end());
+        nums.resize(distance(nums.begin(),it));
+        return nums.size();
+        
+        
+    }
+};

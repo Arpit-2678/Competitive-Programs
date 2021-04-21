cf2408915c767379322e7e3dc599fa2a640c685b
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-04-21 22:06:27 +0530

    Create 717_A_codeforces.cpp

---
+++ 717_A_codeforces.cpp
@@ -0,0 +1,101 @@
+// LINK=https://codeforces.com/contest/1516/problem/A
+
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define test while(t--)
+#define pb push_back
+#define ub upper_bound
+#define lb lower_bound
+#define mp make_pair
+#define xxx exit(0)
+#define ain(n,arr) for(int i=0;i<n;i++){ cin>>arr[i];}     
+#define fs first
+#define sec second
+#define op(container) for(auto it: container) 
+using namespace std;
+const int inf = 1e9+7;
+
+//   for calculating a to power b...
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+//  for calculating gcd.. 
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+//  for converting string to uppercase....
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+//  for converting string to lowercase.... 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    test{
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        ain(n,arr)
+        int s=0,l=n-1;
+        int z_cnt=0;
+        
+        for(int i=0; i<n and arr[i]==0;i++)
+        {
+            z_cnt++;
+        }
+        //cout<<z_cnt<<"\n";
+        s=z_cnt;
+       
+       
+        
+        while(k-- and s<l)
+        {
+            if(arr[s]>0 )
+            {
+                arr[s]--;
+                arr[l]++;
+                
+            }
+           
+            if(arr[s]==0)
+            {
+               s++;
+            //   arr[s]--;
+            //   arr[l]++;
+            }
+             
+        }
+        for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+        cout<<"\n";
+        
+        
+    }
+
+    
+
+	return 0;
+}

f5bfe635d01c28d5e7d378af7aba56ce83769a77
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-04-27 19:21:02 +0530

    Create 1513_A_codeforces.cpp

---
+++ 1513_A_codeforces.cpp
@@ -0,0 +1,102 @@
+// LINK=https://codeforces.com/problemset/problem/1513/A
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define test while(t--)
+#define pb push_back
+#define ub upper_bound
+#define lb lower_bound
+#define mp make_pair
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;     
+#define fs first
+#define sec second
+#define op(container) for(auto it: container)  cout<<it<<" ";
+using namespace std;
+const int inf = 1e9+7;
+
+//   for calculating a to power b...
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+//  for calculating gcd.. 
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+//  for converting string to uppercase....
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+//  for converting string to lowercase.... 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    test{
+        double n,k;
+        cin>>n>>k;
+        if(k>=(ceil)(n/2))
+        {
+            cout<<"-1"<<"\n";
+        }
+        else
+        {
+            std::vector<int>v;
+            int x=n;
+            int y=1;
+            for(int i=0;i<n;i++)
+            {
+                if(!(i&1))
+                {
+                    v.pb(y++);
+                }
+                else
+                {
+                    if(k!=0)
+                    {
+                      v.pb(x--);
+                      k--;
+                    }
+                    else
+                    v.pb(y++);
+                }
+            }
+            for(auto it:v)
+            {
+                cout<<it<<" ";
+            }
+            cout<<"\n";
+        }
+        
+    }
+       
+    
+  
+
+   
+
+    
+
+	return 0;
+}

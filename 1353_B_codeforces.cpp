3bb8168e052d4f4a09b22124fbb6e4d1454a0f5f
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-04-27 18:03:43 +0530

    Create 1353_B_codeforces.cpp

---
+++ 1353_B_codeforces.cpp
@@ -0,0 +1,90 @@
+// LINK=https://codeforces.com/problemset/problem/1353/B
+
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define test while(t--)
+#define pb push_back
+#define ub upper_bound
+#define lb lower_bound
+#define mp make_pair
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;     
+#define fs first
+#define sec second
+#define op(container) for(auto it: container)  cout<<it<<" ";
+using namespace std;
+const int inf = 1e9+7;
+
+//   for calculating a to power b...
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+//  for calculating gcd.. 
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+//  for converting string to uppercase....
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+//  for converting string to lowercase.... 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    test{
+        int n,k;
+        cin>>n>>k;
+        int arr[n]{0};
+        int brr[n]{0};
+        ain(arr) ain(brr)
+        sort(arr,arr+n);
+        sort(brr,brr+n,greater<int>());
+        int i=0;
+        while(k--)
+        {
+            if(arr[i]<brr[i])
+            {
+              swap(arr[i],brr[i]);
+              i++;
+            }
+            else
+            break;
+        }
+        int sum=0;
+        for(auto it:arr)
+        sum+=it;
+        cout<<sum<<"\n";
+    }
+    
+    
+  
+
+   
+
+    
+
+	return 0;
+}

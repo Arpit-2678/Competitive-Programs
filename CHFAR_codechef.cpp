8e8d1dfdcd748d5aa03149b7cb06e2d04fcbae55
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-04-29 23:59:10 +0530

    Create CHFAR_codechef.cpp

---
+++ CHFAR_codechef.cpp
@@ -0,0 +1,28 @@
+// LINK=https://www.codechef.com/problems/CHFAR
+
+#include<iostream>
+using namespace std;
+int main()
+{   
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int n,k;
+       cin>>n>>k;
+       int arr[n]{0};
+       int flag=0;
+       for(int i=0;i<n;i++)
+       {
+           cin>>arr[i];
+           if(arr[i]!=1)
+           flag++;
+       }
+       if(flag>k)
+       {
+           cout<<"NO\n";
+       }
+       else
+       cout<<"YES\n";
+   }
+ }

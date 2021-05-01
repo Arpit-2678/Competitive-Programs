4a4b7e1a1d3307c4476e47f2c8c1330cbf525b9b
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-05-01 21:57:16 +0530

    Create leetcode_1846.cpp

---
+++ leetcode_1846.cpp
@@ -0,0 +1,24 @@
+// LINK=https://leetcode.com/contest/biweekly-contest-51/problems/maximum-element-after-decreasing-and-rearranging/
+
+
+
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+       if(arr[0]!=1)
+       {
+           arr[0]=1;
+       }
+         for(int i=1;i<arr.size();i++)
+         {
+               if(arr[i]-arr[i-1]>1)
+               {
+                    arr[i]=arr[i-1]+1;
+               }
+            
+         }
+       return arr[arr.size()-1];
+    }
+};

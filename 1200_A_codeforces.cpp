ea5c2128acd72931f74fc2116871030ff1cfbc7f
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-05-14 20:13:00 +0530

    Codeforces

--- 1200_A_codeforces.cpp
+++ 1200_A_codeforces.cpp
@@ -1,13 +1,13 @@
 // LINK= https://codeforces.com/problemset/problem/1220/A
 
 #include<bits/stdc++.h>
-#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//#define fastio 
 #define endl "\n"
 #define lli long long int
 
 using namespace std;
 int main()
-{    fastio;
+{    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
  int n;
  cin>>n;
  string str;

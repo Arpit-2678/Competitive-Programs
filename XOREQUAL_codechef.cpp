85457cc2731bf73bb9de4ff299480b705620be92
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-05-17 19:41:00 +0530

    May Long Challenge 2021 Codechef

---
+++ XOREQUAL_codechef.cpp
@@ -0,0 +1,83 @@
+// LINK=https://www.codechef.com/MAY21C/problems/XOREQUAL/
+
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define test while(t--)
+#define pb push_back
+#define ub upper_bound
+#define lb lower_bound
+#define mp make_pair
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;     
+#define fs first
+#define sec second
+#define op(container) for(auto it: container)  cout<<it<<" ";
+#define clr  cin.ignore(numeric_limits<streamsize>::max(), '\n')
+using namespace std;
+const int inf = 1e9+7;
+
+//   for calculating a to power b...
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = ((res%inf) * (a%inf))%inf;
+        a = ((a%inf) * (a%inf))%inf;
+        b >>= 1;
+    }
+    return (res%inf);
+}
+//  for calculating gcd.. 
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+//  for converting string to uppercase....
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+//  for converting string to lowercase.... 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+bool cmp(int a, int b)
+{
+     
+    if (a % 2 == 0 && b % 2 == 0)
+        return a < b;
+ 
+    if (a % 2 != 0 && b % 2 != 0)
+        return b < a;
+ 
+
+    if (a % 2 != 0)
+        return true;
+ 
+    return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll t;
+    cin>>t;
+    test{
+        ll n;
+        cin>>n;
+        ll res=(binpow(2,n-1))%inf;
+        cout<<res<<"\n";
+    }
+     
+    
+
+	return 0;
+}

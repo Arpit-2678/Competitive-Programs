fc78c4b5bed0fecc006c8645b69a46e11d3b724e
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-05-24 01:36:57 +0530

    Leetcode

---
+++ First_Bad_Version.cpp
@@ -0,0 +1,35 @@
+// LINK=https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/774/
+
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+    int i=1;
+    int j=n;                        
+    if(n==1){                         
+        if(isBadVersion(n)){      
+            return n;
+        }
+        return -1;
+    }
+    int ans=-1;
+  while(i<=j){
+      int mid=i+(j-i)/2;
+      if(isBadVersion(mid)){
+          ans=mid;
+          j=mid-1;
+      }
+      else{
+          
+          i=mid+1;
+      }
+      }
+    
+  return ans;  
+        
+       
+    }
+};

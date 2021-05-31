affd478f4e757cbb1f5ac597975c60c5c24862ef
Arpit-2678 <63576280+Arpit-2678@users.noreply.github.com>
2021-06-01 00:40:36 +0530

    Codechef

---
+++ TANDJ1_may_LT_codechef.cpp
@@ -0,0 +1,122 @@
+// LINK=https://www.codechef.com/LTIME96B/problems/TANDJ1
+
+
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define test while(t--)
+#define pb push_back
+#define ub upper_bound
+#define lb lower_bound
+#define mp make_pair
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;     
+#define fs first
+#define sec second
+#define op(container) for(auto it: container)  cout<<it<<" ";
+#define clr  cin.ignore(numeric_limits<streamsize>::max(), '\n')
+using namespace std;
+const int inf = 1e9+7;
+
+//   for calculating a to power b...
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = ((res%inf) * (a%inf))%inf;
+        a = ((a%inf) * (a%inf))%inf;
+        b >>= 1;
+    }
+    return (res%inf);
+}
+//  for calculating gcd.. 
+ll  gcd(ll u, ll v)
+{
+    ll shift;
+    if (u == 0) return v;
+    if (v == 0) return u;
+    shift = __builtin_ctz(u | v);
+    u >>= __builtin_ctz(u);
+    do {
+        v >>= __builtin_ctz(v);
+        if (u > v) {
+          ll t = v;
+            v = u;
+            u = t;
+        }  
+        v = v - u;
+    } while (v != 0);
+    return u << shift;
+}
+//  for converting string to uppercase....
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+//  for converting string to lowercase.... 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+bool cmp(int a, int b)
+{
+     
+    if (a % 2 == 0 && b % 2 == 0)
+        return a < b;
+ 
+    if (a % 2 != 0 && b % 2 != 0)
+        return b < a;
+ 
+
+    if (a % 2 != 0)
+        return true;
+ 
+    return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    test{
+        int a,b,c,d,k;
+        cin>>a>>b>>c>>d>>k;
+        if((abs(c-a)==0 and abs(d-b)==1) or((abs(c-a)==1 and abs(d-b)==0)))
+        {
+            if(k&1)
+            cout<<"YES\n";
+            else
+            cout<<"NO\n";
+        }
+        else
+        {
+               int x=abs(c-a)+abs(d-b);
+               
+                  if(k>=x)
+                  {
+                      if(x&1 and k&1)
+                      {
+                          cout<<"Yes\n";
+                      }
+                      else if(!(x&1) and !(k&1))
+                      {
+                          cout<<"Yes\n";
+                      }
+                      else
+                      cout<<"NO\n";
+                  }
+                  else
+                  cout<<"NO\n";
+        }
+    }
+     
+    
+
+	return 0;
+}

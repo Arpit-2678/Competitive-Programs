7444d3fbc232fd999189da49678d0474a703bbcc
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-06-19 00:32:48 +0530

    Geeks For Geeks

---
+++ Minimum Depth of a Binary Tree.cpp
@@ -0,0 +1,161 @@
+// LINK=https://practice.geeksforgeeks.org/problems/minimum-depth-of-a-binary-tree/1/?category[]=Tree&category[]=Tree&problemStatus=unsolved&problemType=functional&difficulty[]=-2&difficulty[]=-1&difficulty[]=0&page=1&sortBy=submissions&query=category[]TreeproblemStatusunsolvedproblemTypefunctionaldifficulty[]-2difficulty[]-1difficulty[]0page1sortBysubmissionscategory[]Tree#
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+ // } Driver Code Ends
+/* The Node structure is
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+class Solution{
+  public:
+    /*You are required to complete this method*/
+    int bfs(Node *root)
+  {
+      int h1=1;
+    queue<Node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty())
+    {
+      Node* fr=q.front();
+      if(fr==NULL)
+      {
+           //cout<<"\n";
+           h1++;
+           q.pop();
+           if(!q.empty())
+           {
+            q.push(NULL);
+          }
+      }
+      else
+      {
+        if(fr->left==NULL and fr->right==NULL)
+        {
+             //h1++;
+             return h1;
+        }
+        
+               
+        q.pop();
+          if(fr->left)
+           q.push(fr->left);
+          if(fr->right)
+          q.push(fr->right);
+
+
+
+      }
+  }
+  }
+    int minDepth(Node *root) {
+       if(root==NULL)
+       return 0;
+       return bfs(root);
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        // getline(cin, ch);
+        Solution obj;
+        cout << obj.minDepth(root) << endl;
+        // cout<<"~"<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends

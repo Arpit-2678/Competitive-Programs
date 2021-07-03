9db11e49877336a39f036a3d11d45ce1a80cada1
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-07-03 17:42:07 +0530

    Geeks For Geeks

---
+++ Kth element in Matrix.cpp
@@ -0,0 +1,53 @@
+// LINK=https://practice.geeksforgeeks.org/problems/kth-element-in-matrix/1/?problemStatus=unsolved&problemType=functional&page=1&sortBy=submissions&category[]=Heap&category[]=Map&category[]=set&query=problemStatusunsolvedproblemTypefunctionalpage1sortBysubmissionscategory[]Heapcategory[]Mapcategory[]set#
+
+
+// kth largest element in a 2d array sorted row-wise and column-wise
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+int mat[MAX][MAX];
+int kthSmallest(int mat[MAX][MAX], int n, int k);
+// driver program to test above function
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+    
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                cin>>mat[i][j];
+        int r;
+        cin>>r;
+        cout<<kthSmallest(mat,n,r)<<endl;
+    }
+     // cout << "7th smallest element is " << kthSmallest(mat, 4, 7);
+      return 0;
+}
+// } Driver Code Ends
+
+
+
+
+int kthSmallest(int mat[MAX][MAX], int n, int k)
+{
+     priority_queue<int, vector<int>,greater<int> > pq;
+     for(int i=0;i<n;i++)
+     {
+         for(int j=0;j<n;j++)
+         {
+             pq.push(mat[i][j]);
+         }
+     }
+     int ans=0;
+     while(k--)
+     {
+         ans=pq.top();
+         pq.pop();
+     }
+     return ans;
+   
+}

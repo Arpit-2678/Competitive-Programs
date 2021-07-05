833598bd34fb08c2b909aa2ebf4e9a6feeefe8ee
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-07-05 14:56:29 +0530

    Leetcode

---
+++ Sum of Two Integers.cpp
@@ -0,0 +1,11 @@
+// LINK=https://leetcode.com/problems/sum-of-two-integers/
+
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+   return log2(pow(2,a)*pow(2,b));
+    }
+};

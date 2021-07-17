74c8261f0ab907c4e25d4248a741f2794971c928
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-07-17 14:06:47 +0530

    Interviewbit

---
+++ Pairs With Given Xor.cpp
@@ -0,0 +1,17 @@
+//  LINK=https://www.interviewbit.com/problems/pairs-with-given-xor/
+
+
+int Solution::solve(vector<int> &A, int B) {
+    unordered_set<int>ust;
+    for(auto it:A)
+    {
+        ust.insert(it);
+    }
+    int cnt=0;
+    for(auto it:A)
+    {
+        if(ust.find(it^B)!=ust.end())
+        cnt++;
+    }
+    return cnt/2;
+}

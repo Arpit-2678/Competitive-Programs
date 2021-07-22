a0bd8c304bc22b6a88f87bdbca0ad64eba0a6c77
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-07-22 14:27:59 +0530

    Interviewbit

---
+++ Number of 1 Bits.cpp
@@ -0,0 +1,14 @@
+// LINK=https://www.interviewbit.com/problems/number-of-1-bits/
+
+
+inline int setBitCount(int i) {
+    i = i - ((i >> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+    i = (i + (i >> 4)) & 0x0f0f0f0f;
+    i = i + (i >> 8);
+    i = i + (i >> 16);
+    return i & 0x3f;
+}
+int Solution::numSetBits(unsigned int A) {
+     return setBitCount(A);
+}

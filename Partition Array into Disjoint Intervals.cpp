499232c8f8b5243b3b3dd1ab0bef7828ec19c8cb
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-07-22 14:13:37 +0530

    Leetcode

---
+++ Partition Array into Disjoint Intervals.cpp
@@ -0,0 +1,18 @@
+//  LINK=https://leetcode.com/problems/partition-array-into-disjoint-intervals/
+
+
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& a) {
+      int minNum=a[0],maxNum=a[0];
+        int index=0;
+        for(int i=1;i<a.size();i++){
+            if(a[i]<minNum){
+                index=i;
+                minNum=maxNum;
+            }
+            maxNum=max(a[i],maxNum);
+        }
+        return index+1;
+    }
+};

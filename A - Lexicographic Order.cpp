181f8874a22611e19577e83f56f7f14551277c1b
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-09-04 19:14:06 +0530

    Atcoder

---
+++ A - Lexicographic Order.cpp
@@ -0,0 +1,15 @@
+//  LINK=https://atcoder.jp/contests/abc217/tasks/abc217_a
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   string s,t;
+    cin>>s>>t;
+    if(s<t)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    cout<<"No"<<endl;
+}

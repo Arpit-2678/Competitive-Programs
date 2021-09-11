bb7a4a06ac32d5829c306b5e14da360a8133cdcd
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-09-11 18:15:20 +0530

    Atcoder

---
+++ A - Weather Forecast.cpp
@@ -0,0 +1,25 @@
+// LINK=https://atcoder.jp/contests/abc218/tasks/abc218_a
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+
+	int arr[26];
+      for(int i=0;i<26;i++)
+      {
+            cin>>arr[i];
+      }
+      string s;
+      for(int i=0;i<26;i++)
+      {
+          char ch='a'+arr[i]-1;
+          s.push_back(ch);
+           
+      }
+      cout<<s;
+	return 0;
+}
+

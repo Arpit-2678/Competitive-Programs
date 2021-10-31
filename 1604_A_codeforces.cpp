ebf11a676622c2ac929318f530c1a02b2d97e8ad
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-10-31 11:49:35 +0530

    Codeforces

---
+++ 1604_A_codeforces.cpp
@@ -0,0 +1,19 @@
+//  LINK=https://codeforces.com/contest/1604/problem/A
+
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int t; cin >> t;
+  while (t--) {
+    int n; cin >> n;
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+      int k; cin >> k;
+      ans = max(ans, k - i);
+    }
+    cout << ans << '\n';
+  }
+  return 0;
+}

8c788048fd0b765c759a01299b4b6506248c4eac
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-11-18 11:59:31 +0530

    Codeforces

---
+++ 1592_B_codeforces.cpp
@@ -0,0 +1,17 @@
+// LINK=https://codeforces.com/problemset/problem/1592/B
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int a[200005],b[200005],f;
+int main(){
+	int t,n,i,x;
+	cin>>t;
+	while(t--){
+		cin>>n>>x;f=1;
+		for(i=0;i<n;i++){cin>>a[i];b[i]=a[i];}
+	    sort(b,b+n);
+	    for(i=n-x;i<x;i++){if(b[i]!=a[i]){f=0;}}
+	    f?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	}
+}

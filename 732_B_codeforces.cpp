da13fbb20f5afa95aa644a4f2bd77968702ce7c0
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-12-02 13:30:19 +0530

    Codeforces

---
+++ 732_B_codeforces.cpp
@@ -0,0 +1,82 @@
+//  LINK=https://codeforces.com/contest/732/problem/B
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll long long int
+#define int long long int
+#define pb push_back
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;
+#define op(container) for(auto it: container)  cout<<it<<" ";
+#define clr  cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define sortv(arr) sort(arr.begin(),arr.end())
+#define sorta(arr,n) sort(arr,arr+n)
+#define helper(arr) arr.begin(),arr.end()
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+const int inf = 1e9 + 7;
+
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+
+
+
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+	int arr[n] {0};
+	ain(arr)
+	std::vector<int>ans(n, 0);
+	int cnt = 0;
+	for (int i = 0; i < n - 1; i++)
+	{
+		int sum = arr[i] + arr[i + 1];
+		if (sum < k)
+		{
+			arr[i + 1] += k - sum;
+			cnt += k - sum;
+		}
+	}
+
+	cout << cnt << "\n";
+	op(arr)
+
+
+
+
+}
+
+
+signed main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	// cin >> t;
+
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << "\n";
+
+	return 0;
+
+}

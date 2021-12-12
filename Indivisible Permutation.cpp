6f68306f928db5e777cdedecd73ad410ab15419a
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-12-12 23:39:01 +0530

    Codechef

---
+++ Indivisible Permutation.cpp
@@ -0,0 +1,118 @@
+//  LINK=https://www.codechef.com/START19B/problems/INDIPERM
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll long long int
+#define int long long int
+#define pb push_back
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;
+#define op(container) for(auto it: container)  cout<<it<<" ";
+#define clr  cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define sortv(arr) sort(arr.begin(),arr.end())
+#define sorta(arr,n) sort(arr,arr+n)
+#define helper(arr) arr.begin(),arr.end()
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+const int inf = 1e9 + 7;
+
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+
+
+
+
+
+void solve() {
+
+	int n;
+	cin >> n;
+	vector<int>a, b;
+	for (int i = 1; i <= n; i++)
+	{
+
+		if (i % 2 == 1)
+			a.push_back(i);
+		else
+			b.push_back(i);
+	}
+
+
+	int x = 0, y = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i & 1 and x < b.size())
+		{	cout << b[x] << " ";
+			x++;
+		}
+
+
+
+
+		else if (i % 2 == 0 and a.size() > 0)
+		{
+			cout << a.back() << " ";
+			a.pop_back();
+
+		}
+		else
+		{
+
+			if (a.size() > 0)
+			{
+				cout << a.back() << " ";
+				a.pop_back();
+
+			}
+
+
+			else
+			{
+				cout << b[x] << " ";
+				x++;
+
+
+			}
+
+		}
+
+	}
+	cout << endl;
+}
+
+
+signed main(int argc, char const * argv[])
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	cin >> t;
+
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << "\n";
+
+	return 0;
+
+}
+
+
+

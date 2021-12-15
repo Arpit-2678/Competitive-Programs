16da83a162c069a0a888d9a8ea391f5ba0d295bc
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-12-16 00:06:58 +0530

    Codechef

---
+++ Genes.cpp
@@ -0,0 +1,93 @@
+// LINK=https://www.codechef.com/START20C/problems/GENE01
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll long long int
+#define int long long int
+#define pb push_back
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;
+#define op(container) for(auto it: container)  cout<<it<<" ";
+#define clr  cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define sortv(arr) sort(arr.begin(),arr.end())
+#define sorta(arr,n) sort(arr,arr+n)
+#define helper(arr) arr.begin(),arr.end()
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+const int inf = 1e9 + 7;
+
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+//  for calculating gcd..
+int  gcd(int u, int v)
+{
+	int shift;
+	if (u == 0) return v;
+	if (v == 0) return u;
+	shift = __builtin_ctz(u | v);
+	u >>= __builtin_ctz(u);
+	do {
+		v >>= __builtin_ctz(v);
+		if (u > v) {
+			int t = v;
+			v = u;
+			u = t;
+		}
+		v = v - u;
+	} while (v != 0);
+	return u << shift;
+}
+
+
+
+
+void solve() {
+
+
+	char ch1, ch2;
+	cin >> ch1 >> ch2;
+
+	if (ch1 == ch2)
+		cout << ch1 << "\n";
+	else if (ch1 == 'R' or ch2 == 'R')
+		cout << "R\n";
+	else cout << "B\n";
+
+
+}
+
+
+signed main(int argc, char const * argv[])
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	// cin >> t;
+
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << "\n";
+
+	return 0;
+
+}
+
+
+

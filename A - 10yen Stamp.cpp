ece4e9ab60719a39c246a2f9be84991adb4d9a37
Arpit-2678 <arpitdwivedi2611@gmail.com>
2021-12-26 12:03:13 +0530

    Atcoder

---
+++ A - 10yen Stamp.cpp
@@ -0,0 +1,80 @@
+// LINK=https://atcoder.jp/contests/abc233/tasks/abc233_a
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll long long int
+// #define int long long int
+#define pb push_back
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;
+#define op(container) for(auto it: container)  cout<<it<<" ";
+#define clr  cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define sortv(arr) sort(arr.begin(),arr.end())
+#define sorta(arr,n) sort(arr,arr+n)
+#define helper(arr) arr.begin(),arr.end()
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+const int inf = 1e9 + 7;
+
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+
+void solve() {
+
+
+	int x, y;
+	cin >> x >> y;
+	if (x >= y)
+	{
+		cout << 0 << "\n";
+		return;
+	}
+
+	int diff = y - x;
+
+	if (diff % 10 == 0) {
+		cout << diff / 10 << "\n";
+	}
+	else {
+		int x = diff / 10;
+		cout << x + 1 << "\n";
+	}
+
+}
+
+
+signed main(int argc, char const * argv[])
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	// cin >> t;
+
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << "\n";
+
+	return 0;
+
+}
+
+
+
